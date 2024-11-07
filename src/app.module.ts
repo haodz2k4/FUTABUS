6731c68237c02eaf29c6081f75dc4b43871a52d9
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-11-07 15:47:20 +0700

    Connect to mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: function(configService: ConfigService) {
+        return {
+          type: 'mysql',
+          host: configService.get<string>('MYSQL_HOST'),
+          port: configService.get<number>('MYSQL_PORT'),
+          username: configService.get<string>('MYSQL_USERNAME'),
+          password: configService.get<string>('MYSQL_PASSWORD'),
+          database: configService.get<string>('MYSQL_DATABASE'),
+          entities: [],
+          synchronize: true,
+        }
+      }
+    })
+  ],
   controllers: [],
   providers: [],
 })
